34914f8ea7e92804e38258e405d556fa94ce1874
Austin Flores <austin3flores@gmail.com>
2023-11-12 20:13:09 -0800

    First Flask App

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
-import os
+import os, sys
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, world!'
+
+if __name__ == '__main__':
+    hello_world()
