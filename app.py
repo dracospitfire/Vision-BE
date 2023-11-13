e46767396c9157dc72e78e69340c5e7a222502b0
Austin Flores <austin3flores@gmail.com>
2023-11-12 20:51:01 -0800

    added main.py

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 import sys
 from gunicorn.app.wsgiapp import run
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, world!'
 
 if __name__ == '__main__':
     sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app".split()
